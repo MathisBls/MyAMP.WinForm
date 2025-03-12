a64bb4b95f4014f5a06190cc48134eb0071c2b9b
Mathis Bls <mathis.oxym@gmail.com>
2025-03-12 21:34:05 +0100

    Merge branch 'main' of https://github.com/MathisBls/MyAMP.WinForm

--- MyAMP/obj/Debug/net8.0-windows/MyAMP.AssemblyInfo.cs
+++ MyAMP/obj/Debug/net8.0-windows/MyAMP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAMP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc88fafb23830dee08d68636a72f0658400f1c3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4132f8cdc75aa2129a0abf8ec025ed213d75822")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAMP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAMP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
